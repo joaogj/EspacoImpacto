9ebf0726d6ff0d23b7e5102dbf3033c6f85532ad
joaogj <joaogjesus.silva@gmail.com>
2019-08-28 20:32:53 -0300

    segundo teste de console

--- Assets/Scripts/AdController.cs
+++ Assets/Scripts/AdController.cs
@@ -18,7 +18,7 @@ public class AdController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Log.d("oie")
+        Log.d("oi Paulo")
     }
 
     public void StartAd () 
